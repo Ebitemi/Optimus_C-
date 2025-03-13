2ac294baa278c02ec058f2d9b6662fa4c96ffd73
Ebitemi <ebireritemisan@gmail.com>
2025-03-13 08:16:54 +0100

    okay

---
+++ Desktop/C# Assignment/TemsBlogAPI/Data/TemsBlogContext.cs
@@ -0,0 +1,14 @@
+﻿using TemsBlogAPI.Models
+using Microsoft.EntityFrameworkCore;
+
+namespace TemsBlogAPI.Data
+{
+    public class TemsBlogContext : DbContext // ✅ Add : DbContext
+    {
+        public TemsBlogContext(DbContextOptions<TemsBlogContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
